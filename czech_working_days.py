4e1925e42ca5017519677800baa640b5bff0f23b
Swing95 <gamba.d@seznam.cz>
2023-04-20 23:08:07 +0200

    get_holidays and requirements

--- czech_working_days.py
+++ czech_working_days.py
@@ -1,4 +1,166 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta, date
+from dateutil.easter import easter
 
-import czech_holidays
+
+def get_holidays(year):
+    holidays = (
+        {
+            "holiday_name_cz": "Den obnovy samostatného českého státu",
+            "holiday_name_en": "Restoration Day of the Independent Czech State",
+            "date": date(year, 1, 1),
+            "fixed": True,
+            "public_other": "State",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Nový Rok",
+            "holiday_name_en": "New Year's Day",
+            "date": date(year, 1, 1),
+            "fixed": True,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Svátek práce",
+            "holiday_name_en": "Labor Day",
+            "date": date(year, 5, 1),
+            "fixed": True,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den vítězství",
+            "holiday_name_en": "Victory Day",
+            "date": date(year, 5, 8),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den slovanských věrozvěstů Cyrila a Metoděje",
+            "holiday_name_en": "Saints Cyril and Methodius Day",
+            "date": date(year, 7, 5),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den upálení mistra Jana Husa",
+            "holiday_name_en": "Jan Hus Day",
+            "date": date(year, 7, 6),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den české státnosti",
+            "holiday_name_en": "Czech Statehood Day",
+            "date": date(year, 9, 28),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den vzniku samostatného československého státu",
+            "holiday_name_en": "Czechoslovak Independence State Day",
+            "date": date(year, 10, 28),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Den boje za svobodu a demokracii a Mezinárodní den studentstva",
+            "holiday_name_en": "Struggle for freedom and democracy and International Student Day",
+            "date": date(year, 11, 17),
+            "fixed": True,
+            "public_other": "Public",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Štědrý den",
+            "holiday_name_en": "Christmas Eve",
+            "date": date(year, 12, 24),
+            "fixed": True,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "1. svátek vánoční",
+            "holiday_name_en": "Christmas Day",
+            "date": date(year, 12, 25),
+            "fixed": True,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "2. svátek vánoční",
+            "holiday_name_en": "St. Stephen's Day",
+            "date": date(year, 12, 26),
+            "fixed": True,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Velký pátek",
+            "holiday_name_en": "St. Stephen's Day",
+            "date": easter(year) + timedelta(days=-2),
+            "fixed": False,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+        {
+            "holiday_name_cz": "Velikonoční pondělí",
+            "holiday_name_en": "Easter Monday",
+            "date": easter(year) + timedelta(days=1),
+            "fixed": False,
+            "public_other": "Other",
+            "working_day": False,
+            "description_en": "",
+            "description_cz": ""
+        },
+
+    )
+
+    return holidays
+
+
