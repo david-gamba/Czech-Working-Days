feaa2a59ea7c08520c2e329ccea1d62db1e50deb
Swing95 <gamba.d@seznam.cz>
2023-04-22 12:33:38 +0200

    new tests

--- test/tests.py
+++ test/tests.py
@@ -1,7 +1,8 @@
 import unittest
 from datetime import date, timedelta
 from dateutil.easter import easter
-from czech_workdays_holidays import get_holidays
+from czech_workdays_holidays import get_holidays, get_workdays, get_shopping_days, get_holidays_during_weekend
+from random import randint
 
 
 class TestGetHolidays(unittest.TestCase):
@@ -13,6 +14,29 @@ class TestGetHolidays(unittest.TestCase):
                              date(year, 10, 28), date(year, 11, 17), date(year, 12, 24), date(year, 12, 25),
                              date(year, 12, 26)}
         holidays = set(get_holidays(year, dates_only=True))
+        self.assertSetEqual(holidays, expected_holidays)
+
+    def test_good_friday_before_2016(self):
+        year = randint(2001, 2015)
+        expected_holidays = {date(year, 1, 1), easter(year) + timedelta(days=1),
+                             date(year, 5, 1), date(year, 5, 8), date(year, 7, 5), date(year, 7, 6), date(year, 9, 28),
+                             date(year, 10, 28), date(year, 11, 17), date(year, 12, 24), date(year, 12, 25),
+                             date(year, 12, 26)}
+        holidays = set(get_holidays(year, dates_only=True))
+        self.assertSetEqual(holidays, expected_holidays)
+
+    def test_shopping_restriction_before_2016(self):
+        year = randint(2001, 2015)
+
+        expected_result = Exception("Shopping resctriction came into effect 01/10/2016 (DD/MM/YY)")
+
+        with self.assertRaises(Exception):
+            get_holidays(year, shopping_restricted=True)
+
+    def test_shopping_restriction_during_2016(self):
+        year = 2016
+        expected_holidays = {date(year, 10, 28), date(year, 12, 25), date(year, 12, 26)}
+        holidays = set(get_holidays(year, dates_only=True,shopping_restricted=True))
         self.assertSetEqual(holidays, expected_holidays)
 
     def test_dates_and_cz_names(self):
@@ -53,7 +77,10 @@ class TestGetHolidays(unittest.TestCase):
         holidays = get_holidays(year, dates_and_en_names=True)
         self.assertEqual(holidays, expected_holidays)
 
+
 class TestAssertRaises(unittest.TestCase):
+
+    # Get holidays
     def test_year_type(self):
         with self.assertRaises(TypeError):
             get_holidays('2023')
@@ -74,7 +101,7 @@ class TestAssertRaises(unittest.TestCase):
         with self.assertRaises(TypeError):
             get_holidays(2023, shopping_restricted='False')
 
-    def test_year_value(self):
+    def test_year_value_get_holidays(self):
         with self.assertRaises(Exception):
             get_holidays(2000)
 
@@ -101,3 +128,54 @@ class TestAssertRaises(unittest.TestCase):
         holidays = get_holidays(2023, shopping_restricted=True)
         for holiday in holidays:
             self.assertTrue(holiday['shopping_restricted'])
+
+    # Get workdays
+    def test_get_workdays(self):
+        # Test case 1: Year must be an integer
+        try:
+            get_workdays("2023")
+        except TypeError as e:
+            assert str(e) == "Year must be an integer."
+
+        # Test case 2: include_saturday must be a boolean
+        try:
+            get_workdays(2023, include_saturday="True")
+        except TypeError as e:
+            assert str(e) == "include_saturday must be a boolean."
+
+        # Test case 3: include_sunday must be a boolean
+        try:
+            get_workdays(2023, include_sunday="True")
+        except TypeError as e:
+            assert str(e) == "include_sunday must be a boolean."
+
+        # Test case 4: include_holidays must be a boolean
+        try:
+            get_workdays(2023, include_holidays="True")
+        except TypeError as e:
+            assert str(e) == "include_holidays must be a boolean."
+
+    # Get shopping days
+    def test_year_type_exception(self):
+        with self.assertRaises(TypeError):
+            get_shopping_days("2023")
+
+    def test_include_saturday_type_exception(self):
+        with self.assertRaises(TypeError):
+            get_shopping_days(2023, include_saturday="True")
+
+    def test_include_sunday_type_exception(self):
+        with self.assertRaises(TypeError):
+            get_shopping_days(2023, include_sunday=1)
+
+    def test_exclude_shopping_restricted_days_type_exception(self):
+        with self.assertRaises(TypeError):
+            get_shopping_days(2023, exclude_shopping_restriced_days=0)
+
+    # Get holidays during weekend
+    def test_get_holidays_during_weekend_year_type_exception(self):
+        # Test case 1: Year must be an integer
+        try:
+            get_holidays_during_weekend("2023")
+        except TypeError as e:
+            assert str(e) == "Year must be an integer."
